82148039752d0cc171878f8224b334472a948995
nhantruongthanh123 <thanhnhanqs2005@gmail.com>
2025-12-06 09:20:24 +0700

    Update more books

--- app/models/HomeModel.php
+++ app/models/HomeModel.php
@@ -3,12 +3,12 @@
 class HomeModel extends BaseModel {
     protected $table = 'books';
 
-    public function getListBooks($limit = 10){
+    public function getListBooks($limit = 8){
         $sql = "SELECT b.*, a.author_name, c.category_name
                 FROM {$this->table} b
                 LEFT JOIN authors a ON b.author_id = a.author_id
                 LEFT JOIN categories c ON b.category_id = c.category_id
-                WHERE b.is_active = 1
+                WHERE b.is_active = 1 AND b.is_featured = 1
                 ORDER BY b.created_at DESC 
                 LIMIT :limit";
 
